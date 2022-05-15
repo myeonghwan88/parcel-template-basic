67a3ee6d68a4b1f54321cf417df8becac7d62fbe
myeonghwan88 <idmash88@gmail.com>
2022-05-15 19:16:29 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,21 @@
+// ESM
+// CommonJS
+
+//import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+//export {
+// plugin: [
+//   autoprefixer
+//  ]
+// }
+// module.exports = {
+//   plugin: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugin: [
+    require('autoprefixer')
+  ]
+}
